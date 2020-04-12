069d51d27161d8e4fcd11d106ed7b727fddadb16
MarcosJunior8 <marcos.junior2@alunos.unis.edu.br>
2020-04-12 20:21:45 -0300

    Jogo de Futebol - POO 4

---
+++ atividade 4 - POO/Jogador.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace atividade_4___POO
+{
+    class Jogador
+    {
+        private string nome;
+        private int idade;
+        private int habilidade;
+        private int camisa;
+        private int gols;
+
+
+        public Jogador(string xnome, int xidade, int xhabilidade, int xcamisa)
+        {
+            this.nome = xnome;
+            this.idade = xidade;
+            this.habilidade = xhabilidade;
+            this.camisa = xcamisa;
+        }
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public int getCamisa()
+        {
+            return camisa;
+        }
+
+        public int getHabilidade()
+        {
+            return habilidade;
+        }
+
+        public int Gols
+        {
+          get { return gols; }
+          set { gols += value; }
+        }
+
+        public string getDescricao()
+        {
+            return "Nome: " + this.getNome() +
+                    "\tIdade: " + this.idade +
+                    "\tHabilidade: " + this.getHabilidade() +
+                    "\tCamisa : " + this.getCamisa() +
+                    "\tGols: " + this.Gols;
+        }
+
+        
+    }
+}
---
+++ atividade 4 - POO/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace atividade_4___POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Time time1 = new Time("Flamengo");
+            time1.contratarJogador(new Jogador("Paulo", 20,94,1));
+            time1.contratarJogador(new Jogador("Alex", 20,93,2 ));
+            time1.contratarJogador(new Jogador("João", 25,90,3));
+            time1.contratarJogador(new Jogador("Douglas", 22,85,4));
+            time1.contratarJogador(new Jogador("John", 21,98,5));
+            time1.contratarJogador(new Jogador("Pedro", 29,99,6));
+            time1.contratarJogador(new Jogador("Carlos", 19,87,7));
+            time1.contratarJogador(new Jogador("Xavier" , 27,88,8));
+            time1.contratarJogador(new Jogador("Dendi", 26,92,9));
+            time1.contratarJogador(new Jogador("Filipe", 33,91,10));
+            time1.contratarJogador(new Jogador("Taylor", 32,95,11));
+
+
+
+            Time time2 = new Time("Cruzeiro");
+            time2.contratarJogador(new Jogador("Creyson", 20, 94, 1));
+            time2.contratarJogador(new Jogador("Lesley", 20, 93, 2));
+            time2.contratarJogador(new Jogador("Japa", 25, 90, 3));
+            time2.contratarJogador(new Jogador("Pele", 22, 85, 4));
+            time2.contratarJogador(new Jogador("Marcos", 21, 98, 5));
+            time2.contratarJogador(new Jogador("Jean", 29, 99, 6));
+            time2.contratarJogador(new Jogador("Rafael", 19, 87, 7));
+            time2.contratarJogador(new Jogador("Wagner", 27, 88, 8));
+            time2.contratarJogador(new Jogador("Yago", 26, 92, 9));
+            time2.contratarJogador(new Jogador("Cassio", 33, 91, 10));
+            time2.contratarJogador(new Jogador("Zagal", 32, 95, 11));
+
+            Partida p1 = new Partida(DateTime.Now, time1, time2);
+            p1.jogar();
+            Console.WriteLine(p1.getPlacar());
+
+            Console.WriteLine(time1.getStatus());
+            time1.getDescricaoJogadores();
+
+
+            Console.WriteLine(time2.getStatus());
+            time2.getDescricaoJogadores();
+
+            Console.ReadKey();
+
+        }
+    }
+}
